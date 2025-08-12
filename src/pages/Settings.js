24985fc534079863881f464517dc9cdfa357d72f
hujadis <64236635+hujadis@users.noreply.github.com>
2025-08-12 11:09:25 +0300

    v3

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Settings as SettingsIcon, Upload, Download, Trash2, Save } from 'lucide-react';
+import { Download, Trash2, Save } from 'lucide-react';
 import toast from 'react-hot-toast';
 
 const Settings = () => {
